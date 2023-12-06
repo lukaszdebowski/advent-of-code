22a4fe6df3f59a07eb3946030cf016204fc3b357
Łukasz Dębowski <lukasz.debowski@htdevelopers.com>
2023-12-06 16:54:05 +0100

    cleanup

--- day6/day6.go
+++ day6/day6.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type Race struct {
 	time     int
@@ -10,20 +8,11 @@ type Race struct {
 }
 
 func main() {
-	resultPart1 := getResultFromRaces([]Race{
-		{time: 56, distance: 499},
-		{time: 97, distance: 2210},
-		{time: 77, distance: 1097},
-		{time: 93, distance: 1440},
-	})
-
-	resultPart2 := getResultFromRaces([]Race{
-		{time: 56977793, distance: 499221010971440},
-	})
+	resultPart1 := getResultFromRaces([]Race{{time: 56, distance: 499}, {time: 97, distance: 2210}, {time: 77, distance: 1097}, {time: 93, distance: 1440}})
+	resultPart2 := getResultFromRaces([]Race{{time: 56977793, distance: 499221010971440}})
 
 	fmt.Println("resultPart1", resultPart1)
 	fmt.Println("resultPart2", resultPart2)
-
 }
 
 func getResultFromRaces(races []Race) int {
