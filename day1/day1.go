fadb52a83df7a0ab169253f7501dbdf527274067
Łukasz Dębowski <lukasz.debowski@htdevelopers.com>
2023-12-08 13:54:09 +0100

    missing day1

--- day1/day1.go
+++ day1/day1.go
@@ -2,8 +2,48 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 func main() {
-	fmt.Println("hello world")
+	data, _ := os.ReadFile("example.txt")
+	lines := strings.Split(string(data), "\n")
+
+	result := 0
+
+	for _, line := range lines {
+		firstDigit := getFirstDigit(line)
+		lastDigit := getLastDigit(line)
+
+		value := 10*firstDigit + lastDigit
+		result += value
+	}
+
+	fmt.Println("Result:", result)
+}
+
+func getFirstDigit(line string) int {
+	for _, char := range line {
+		if unicode.IsNumber(char) {
+			number, _ := strconv.Atoi(string(char))
+			fmt.Println("Found first number:", number)
+			return number
+		}
+	}
+	panic("No number found in line")
+}
+
+func getLastDigit(line string) int {
+	for i := len(line) - 1; i >= 0; i-- {
+		if unicode.IsNumber(rune(line[i])) {
+			number, _ := strconv.Atoi(string(line[i]))
+			fmt.Println("Found last number:", number)
+			return number
+		}
+	}
+	panic("No number found in line")
+
 }
